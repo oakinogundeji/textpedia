5dd96ad2fd311170752f51944c3779b247b82db7
Muyiwa Akin-Ogundeji <oakinogundeji@gmail.com>
2016-08-14 20:40:13 +0100

    adding SMS sending features

--- utils/messaging_send.js
+++ utils/messaging_send.js
@@ -2,22 +2,19 @@
  * Module Dependencies
  */
 //=============================================================================
-/*const
-  plivo = require('plivo'),
-  config = require('../config/config'),
-  p = plivo.RestAPI({
-  authId: config.plivo.ID,
-  authToken: config.plivo.token
-});*/
+const
+  twilio = require('twilio'),
+  config = require('../config/config');
 //=============================================================================
 /**
  * Module variables
  */
 //=============================================================================
-/*const
-  src = config.plivo.src,
-  send_url = config.plivo.send_url,
-  mthd = 'GET';*/
+const
+  acctSID = config.twilio.testAccountSid,
+  authToken = config.twilio.testAuthToken,
+  sender = config.twilio.testPhoneNumber,
+  client = new twilio.RestClient(acctSID, authToken);
 //=============================================================================
 /**
  * Export Module
@@ -25,21 +22,21 @@
 //=============================================================================
 module.exports = function (p_num) {
   send_params = {
-    'src': src, // Sender's phone number with country code
-    'dst': p_num, // Receiver's phone Number with country code
-    'text': "Hi, your research results have been sent to your email address." +
-      "Thank you for using the Textpedia service.", // Your SMS Text Message - English
-    //'text' : "こんにちは、元気ですか？", // Your SMS Text Message - Japanese
-    //'text' : "Ce est texte généré aléatoirement", // Your SMS Text Message - French
-    'url': send_url, // The URL to which with the status of the message is sent
-    'method': mthd // The method used to call the url
+    'to': p_num, // Sender's phone number with country code
+    'from': sender, // Receiver's phone Number with country code
+    'body': "Hi, your research results have been sent to your email address." +
+      "Thank you for using the Textpedia service."
 };
   // Prints the complete response
-  p.send_message(send_params, function (status, resp) {
-      console.log('Status: ', status);
-      console.log('API Response:\n', resp);
-      console.log('Message UUID:\n', resp['message_uuid']);
-      console.log('Api ID:\n', resp['api_id']);
+  client.messages.create(send_params, function (err, resp) {
+    if(err) {
+      console.log('There was an error sending the message via Twilio');
+      return console.error(err);
+    }
+    else {
+      console.log('The response from Twilio');
+      return console.log(resp);
+    }
   });
 };
 //=============================================================================
