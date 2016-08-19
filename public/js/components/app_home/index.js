57ecc8e696d4bffda3f679361767561eae0fbaf7
Muyiwa Akin-Ogundeji <oakinogundeji@gmail.com>
2016-08-19 16:28:03 +0100

    providing text for why textpedia

--- public/js/components/app_home/index.js
+++ public/js/components/app_home/index.js
@@ -2,6 +2,11 @@
 module.exports = function ($) {
   return {
     template: require('./template.html'),
+    methods: {
+      signup: function () {
+        return this.$dispatch('show-signup');
+      }
+    },
     ready: function () {
     }
   };
