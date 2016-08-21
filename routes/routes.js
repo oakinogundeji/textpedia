8a0c3d45de2070f9248629768046f946866aab2e
Muyiwa Akin-Ogundeji <oakinogundeji@gmail.com>
2016-08-21 21:34:52 +0100

    split user creation functionality off into separate module

--- routes/routes.js
+++ routes/routes.js
@@ -5,10 +5,9 @@
 //=============================================================================
 const
   cr8search = require('../utils/cr8search'),
-  sendToken = require('../utils/sendToken'),
+  cr8User = require('../utils/cr8User'),
   express = require('express'),
   router = express.Router();
-var User = require('../models/users');
 //=============================================================================
 /**
  * Module variables
@@ -16,40 +15,6 @@ var User = require('../models/users');
 //=============================================================================
 const INBOUND_RESP = '<?xml version="1.0" encoding="UTF-8" ?>' +
   '<Response></Response>';
-
-function generateRandomToken() {
-  var
-    date = new Date().getTime(),
-    xterBank = 'abcdefghijklmnopqrstuvwxyz',
-    fstring = '',
-    i;
-  for(i = 0; i < 15; i++) {
-    fstring += xterBank[parseInt(Math.random()*26)];
-  }
-  return (fstring += date);
-}
-
-function cr8NewUser(p_num, email, res) {
-  let newUser = new User();
-  newUser.phoneNumber = p_num;
-  newUser.email = email;
-  newUser.active = false;
-  newUser.temp_token = {
-    value: generateRandomToken(),
-    time: Date.now()
-  };
-  newUser.save(function (err, user) {
-    if(err) {
-      console.log('There was an error saving the created user');
-      console.error(err);
-      throw err;
-    }
-    else {
-      console.log('new user successfully created', user);
-      return sendToken(user.temp_token.value, user.email, res);
-    }
-  });
-}
 //=============================================================================
 /**
  * Routes
@@ -64,29 +29,7 @@ router.post('/submit', function (req, res) {
   const
     EMAIL = req.body.data.email,
     PHONE_NUM = req.body.data.phoneNumber;
-  User.findOne({phoneNumber: PHONE_NUM}, function (err, user) {
-    if(err) {
-      console.log('There was a db access error');
-      console.error(err);
-      throw err;
-    }
-    if(user && (Date.now() - new Date(user.temp_token.time).getTime() <  2 * 60 * 60 * 1000)) {
-      return res.status(409).json('Phone number already in use!');
-    }
-    else if(user && (Date.now() - new Date(user.temp_token.time).getTime() >=  2 * 60 * 60 * 1000)) {
-      user.remove(function (err) {
-        if(err) {
-          console.log('There was an error removinf old user record from dB');
-          console.error(err);
-          throw err;
-        }
-        return cr8NewUser(PHONE_NUM, EMAIL, res);
-      });
-    }
-    else {
-      return cr8NewUser(PHONE_NUM, EMAIL, res);
-    }
-  });
+  return cr8User(EMAIL, PHONE_NUM, res);
 });
 router.post('/confirm', function (req, res) {
   console.log('confirm data from vue', req.body.data);
---
+++ utils/cr8User.js
@@ -0,0 +1,77 @@
+'use strict';
+/**
+ * Module dependencies
+ */
+//=============================================================================
+const
+  sendToken = require('./sendToken'),
+  User = require('../models/users');
+//=============================================================================
+/**
+ * Module variables
+ */
+//===========================================================================
+function generateRandomToken() {
+  var
+    date = new Date().getTime(),
+    xterBank = 'abcdefghijklmnopqrstuvwxyz',
+    fstring = '',
+    i;
+  for(i = 0; i < 15; i++) {
+    fstring += xterBank[parseInt(Math.random()*26)];
+  }
+  return (fstring += date);
+}
+
+function cr8NewUser(p_num, email, res) {
+  let newUser = new User();
+  newUser.phoneNumber = p_num;
+  newUser.email = email;
+  newUser.active = false;
+  newUser.temp_token = {
+    value: generateRandomToken(),
+    time: Date.now()
+  };
+  newUser.save(function (err, user) {
+    if(err) {
+      console.log('There was an error saving the created user');
+      console.error(err);
+      throw err;
+    }
+    else {
+      console.log('new user successfully created', user);
+      return sendToken(user.temp_token.value, user.email, res);
+    }
+  });
+}
+//=============================================================================
+/**
+ * Export module
+ */
+//=============================================================================
+module.exports = function (email, p_num, res) {
+  User.findOne({phoneNumber: p_num}, function (err, user) {
+    if(err) {
+      console.log('There was a db access error');
+      console.error(err);
+      throw err;
+    }
+    if(user && (Date.now() - new Date(user.temp_token.time).getTime() <  2 * 60 * 60 * 1000)) {
+      return res.status(409).json('Phone number already in use!');
+    }
+    else if(user && (Date.now() - new Date(user.temp_token.time).getTime() >=  2 * 60 * 60 * 1000)) {
+      user.remove(function (err) {
+        if(err) {
+          console.log('There was an error removing old user record from dB');
+          console.error(err);
+          throw err;
+        }
+        return cr8NewUser(p_num, email, res);
+      });
+    }
+    else {
+      return cr8NewUser(p_num, email, res);
+    }
+  });
+};
+//=============================================================================
